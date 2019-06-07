0f5267e88a071f1c78f3025f9c0df3673dba0d7b
Wen Chien Yen <cyen7115@berkeley.edu>
2019-06-07 00:36:40 -0700

    Update 06-06-19 exercises and concept checks

---
+++ 06-06-2019-lec/while-loop-exercises.js
@@ -0,0 +1,50 @@
+/* Exercise 0: Houston Count Down to 0
+   Program Specs: Write a program, using a while loop, that prints out the following pattern
+                  to the console. Starting from "Houston, count down: 10", all the way down
+                  to "Houston, count down: 0"
+   Program Output:
+    >>> "Houston, count down: 10"
+    >>> "Houston, count down: 9"
+    ...
+    >>> "Houston, count down: 0"
+ */
+
+/* Exercise 1: Houston Count Down, and Blast Off
+   Program Specs: Write a program, using a while loop, similar to exercise 1, except when the count
+           is 0, you want to print out, "BlAST OFF ! > > > > !"
+   Program Output:
+    >>> "Houston, count down: 10"
+    >>> "Houston, count down: 9"
+    ...
+    >>> "Houston, count down: 1"
+    >>> "BLAST OFF ! > > > > !"
+/* 
+
+/* Exercise 2: Print Triangle
+   Program Specs: Write a loop that makes seven calls to console.log to 
+                  output the following triangle:
+ */
+
+/* Exercise 3: "Fizz", "Buzz"
+   Program Specs: Write a program that uses console.log to print all the numbers
+                  from 1 to 100, with two exceptions.
+                  For numbers divisible by 3, print "Fizz" instead of the number, 
+                  and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+ */
+
+ /* Exercise 4: "Fizz", "Buzz", "FizzBuzz"
+    Program Specs: Modify your program to print "FizzBuzz" for numbers that are divisible
+                   by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible 
+                   by only one of those).
+  */
+
+ /* Exercise 5: Is 97 a prime number? 
+    Program Specs: Without checking online whether 97 is a prime number or not, write 
+                   a pogram that determines whether 97 is a prime number. If your program
+                   determines that 97 is a prime number, 
+                   print to the console, "97 is a prime number!",
+                   otherwise, print to the console. "97 is not a prime number!"
+
+    Note: Try this out with different numbers and see what your result will be
+  */
+
